4def65294c5e8e9c4c0861a100556f553180782b
mrshukhratbek <mrshukhratbek>
2022-07-02 05:42:34 +0500

    fix: rendering todos

--- src/pages/todos/todos.component.jsx
+++ src/pages/todos/todos.component.jsx
@@ -7,14 +7,12 @@ export const Todos = hoc(useTodosProps, ({ todos }) => {
     <div className="container-sm p-4 p-md-0 pt-md-5">
       <TodosInput />
       <div className="row offset-md-3 offset-lg-4 mt-2">
-        {todos.length > 0 && (
-          <ul className="col-md-8 col-lg-6 list-unstyled shadow-sm border p-0">
-            {todos.map((todo) => (
-              <TodosItem key={todo.id} todo={todo} />
-            ))}
-            <TodosActions count={todos.length} />
-          </ul>
-        )}
+        <ul className="col-md-8 col-lg-6 list-unstyled shadow-sm border p-0">
+          {todos.map((todo) => (
+            <TodosItem key={todo.id} todo={todo} />
+          ))}
+          <TodosActions count={todos.length} />
+        </ul>
       </div>
     </div>
   );
